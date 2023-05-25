b1d422aa0064f5de3dce564f1bbf5a60d6fc5d08
bitmagier <bitmagier@mailbox.org>
2023-05-25 16:00:46 +0200

    added deccomp:unconfined to work around user namespace changing issue in chrome/idea/opera

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 source .env
-docker run -it -p 23:22 --mount type=bind,source="$(pwd)"/persistent_home,target=/home/$USER_NAME mate-dev-docker-ubuntu /bin/bash
+docker run -it -p 23:22 --security-opt seccomp=unconfined --mount type=bind,source="$(pwd)"/persistent_home,target=/home/$USER_NAME mate-dev-docker-ubuntu /bin/bash
